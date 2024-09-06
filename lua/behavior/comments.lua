a4f99782f4538587da1816e2d19dc9dc76dca2b7
Rafael Zuquim <rzuquim@gmail.com>
2024-09-06 20:00:43 -0300

    simpler ts prehook on comments plugin

--- lua/behavior/comments.lua
+++ lua/behavior/comments.lua
@@ -10,21 +10,9 @@ local M = {
 }
 
 function M.config()
+    local prehook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
     require('Comment').setup(vim.tbl_deep_extend('force', KEYMAPS.comments(), {
-        pre_hook = function(ctx)
-            local U = require('Comment.utils')
-            local location = nil
-            if ctx.ctype == U.ctype.block then
-                location = require('ts_context_commentstring.utils').get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-                location = require('ts_context_commentstring.utils').get_visual_start_location()
-            end
-
-            return require('ts_context_commentstring.internal').calculate_commentstring({
-                key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-                location = location,
-            })
-        end,
+        pre_hook = prehook,
     }))
 end
 
