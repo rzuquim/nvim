d306069d67c636dcc38d3f414484af13767001ba
Rafael Zuquim <rzuquim@gmail.com>
2024-09-05 23:40:02 -0300

    loading conform langs

--- lua/behavior/conform.lua
+++ lua/behavior/conform.lua
@@ -1,21 +1,38 @@
-return { -- Autoformat
+local langs = require('langs')
+
+local formatters_by_ft = {}
+for _, settings in pairs(langs) do
+    if settings.extra_formatters then
+        for ft, formatter in pairs(settings.extra_formatters) do
+            formatters_by_ft[ft] = formatter
+        end
+    end
+end
+
+local M = {
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-        {
-            '<leader>f',
-            function()
-                require('conform').format({ async = true, lsp_fallback = true })
-            end,
-            mode = '',
-            desc = '[F]ormat buffer',
-        },
-    },
-    opts = {
-        notify_on_error = false,
-        formatters_by_ft = {
-            -- javascript = { { "prettierd", "prettier" } },
-        },
-    },
+    lazy = false,
+    keys = {},
 }
+
+function M.config()
+    local conform = require('conform')
+    conform.setup({
+        notify_on_error = false,
+
+        -- NOTE: disabling format on save
+        --[[ format_on_save = function(bufnr)
+            -- Disable "format_on_save lsp_fallback" for languages that don't
+            local disable_filetypes = { c = true, cpp = true }
+            return {
+                timeout_ms = 500,
+                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            }
+        end, ]]
+
+        formatters_by_ft = formatters_by_ft,
+    })
+    KEYMAPS.format(conform)
+end
+
+return M
