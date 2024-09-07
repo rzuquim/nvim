8971469822a458b364c48c49661d7a780b793960
Rafael Zuquim <rzuquim@gmail.com>
2024-09-07 11:49:51 -0300

    macro recording alert on lualine

--- lua/appearance/lualine.lua
+++ lua/appearance/lualine.lua
@@ -14,6 +14,19 @@ local currentProject = function()
     return last_dir
 end
 
+-- from: https://www.reddit.com/r/neovim/comments/xy0tu1/cmdheight0_recording_macros_message
+-- NOTE: not adding the autocommands to avoid complexity (there is a 1s poll on lua line, so it is not instantaneous)
+vim.api.nvim_set_hl(0, 'MacroRec', { fg = '#db4b4b', bold = true })
+
+local function show_macro_recording()
+    local recording_register = vim.fn.reg_recording()
+    if recording_register == '' then
+        return ''
+    else
+        return '%#MacroRec#󰑋 @' .. recording_register .. '%*'
+    end
+end
+
 local M = {
     'nvim-lualine/lualine.nvim', -- status line
     event = 'VeryLazy',
@@ -24,7 +37,7 @@ local M = {
         sections = {
             lualine_a = { currentProject },
             lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'mode', 'filename' },
+            lualine_c = { { "macro_recording", fmt = show_macro_recording }, "mode", "filename" },
             lualine_x = { 'encoding', 'filetype' },
             lualine_y = { 'progress' },
             lualine_z = { 'location' },
