9f3fa3e63fa8fd3fd68e202cde58893ee494680a
Rafael Zuquim <rzuquim@gmail.com>
2024-10-06 17:29:36 -0300

    disabling xmlformatter (not working after all)

--- lua/langs/xml.lua
+++ lua/langs/xml.lua
@@ -1,8 +1,21 @@
 return {
-    settings = {},
-    extra_formatters = {
-        xml = { 'xmlformatter' },
+    settings = {
+        xml = {
+            validation = {
+                dtd = true,
+                schema = true,
+            },
+            format = {
+                -- TODO: disable when xmlformatter is working
+                enabled = true,
+            },
+        },
     },
+    -- TODO: enable when we can fix the error: 'xmlformatter unavailable: Formatter config missing or incomplete'
+    -- extra_formatters = {
+    --     xml = { 'xmlformatter' },
+    --     config = { 'xmlformatter' },
+    -- },
     extra_treesitter = {
         'xml',
     },
