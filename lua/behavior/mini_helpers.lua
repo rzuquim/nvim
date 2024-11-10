7de889fb138466fa7a98d8faf84a04dd6656cf01
Rafael Zuquim <rzuquim@gmail.com>
2024-11-10 20:30:24 -0300

    simpler mini_surround keymaps

--- lua/behavior/mini_helpers.lua
+++ lua/behavior/mini_helpers.lua
@@ -3,15 +3,28 @@ return {
     config = function()
         -- Better Around/Inside textobjects
         -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-        --  - ci'  - [C]hange [I]nside [']quote
+        --  - vib  - [V]isually select [I]nside brackets
+        --  - ciq  - [C]hange [I]nside quotes
         require('mini.ai').setup({ n_lines = 500 })
 
         -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+        -- - siW) -  [S]urround [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
+        require('mini.surround').setup({
+            mappings = {
+                add = 's',
+                delete = 'sd',
+                replace = 'sr',
+
+                -- NOTE: disabled
+                find = '',
+                find_left = '',
+                highlight = '',
+                update_n_lines = '',
+                suffix_last = '',
+                suffix_next = '',
+            },
+        })
     end,
 }
