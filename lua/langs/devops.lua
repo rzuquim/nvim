7ecf9ff2c8977f96e6081d01518a673c0ab59615
Rafael Zuquim <rzuquim@gmail.com>
2025-01-11 16:29:08 -0300

    nginx lsp

---
+++ lua/langs/devops.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.nginx = {
+    settings = {},
+}
+
+return M
--- lua/langs/init.lua
+++ lua/langs/init.lua
@@ -32,6 +32,7 @@ local M = {
     taplo = require('langs.toml'),
     vtsls = require('langs.js'),
     sqlls = require('langs.sql'),
+    nginx_language_server = require('langs.devops').nginx,
     -- TODO: dockerfile
     -- TODO: gitignore
     -- TODO: sql
