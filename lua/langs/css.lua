7047f77a3e1ab2d6cc95f83c581d6bfaf4688f66
Rafael Zuquim <rzuquim@gmail.com>
2025-07-21 18:54:22 -0300

    enabling tailwind plugin only if the project uses it

--- lua/langs/css.lua
+++ lua/langs/css.lua
@@ -28,9 +28,23 @@ M.tailwind = {
             'luckasRanarison/tailwind-tools.nvim',
             build = ':UpdateRemotePlugins',
             config = function()
+                local cwd = vim.fn.getcwd()
+
+                local config_files = {
+                    'tailwind.config.js',
+                }
+
+                local uses_tailwind = false
+                for _, filename in ipairs(config_files) do
+                    local path = cwd .. '/' .. filename
+                    if vim.loop.fs_stat(path) then
+                        uses_tailwind = true
+                    end
+                end
+
                 require('tailwind-tools').setup({
                     conceal = {
-                        enabled = true,
+                        enabled = uses_tailwind,
                     },
                 })
             end,
