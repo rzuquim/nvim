8cd3354c831ec1163a0e95711d8d9ff9b87f2044
Rafael Zuquim <rzuquim@gmail.com>
2026-03-05 12:57:23 -0300

    open pdf in zathura

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd('CmdwinEnter', {
         vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>', ':q<CR>', { noremap = true, silent = true })
     end,
 })
+
+vim.api.nvim_create_autocmd('BufReadCmd', {
+    pattern = '*.pdf',
+    callback = function()
+        local filename = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
+        vim.cmd('silent !zathura ' .. filename .. ' &')
+        vim.cmd('let tobedeleted = bufnr(\'%\') | b# | exe "bd! " . tobedeleted')
+    end,
+})
