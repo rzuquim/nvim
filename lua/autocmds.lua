0532daa24062f6c2e67516678bf564db6d60cb61
Rafael Zuquim <rzuquim@gmail.com>
2026-03-31 12:50:20 -0300

    opening movies from nvim

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -9,8 +9,27 @@ vim.api.nvim_create_autocmd('CmdwinEnter', {
 vim.api.nvim_create_autocmd('BufReadCmd', {
     pattern = '*.pdf',
     callback = function()
-        local filename = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
+        local filename = vim.fn.expand('%:p')
+        if filename == '' then
+            return
+        end
         vim.cmd('silent !zathura ' .. filename .. ' &')
         vim.cmd('let tobedeleted = bufnr(\'%\') | b# | exe "bd! " . tobedeleted')
+    end,
+})
+
+vim.api.nvim_create_autocmd('BufReadCmd', {
+    pattern = { '*.mp4', '*.mkv', '*.webm', '*.avi', '*.m4a', '*.mp3', '*.ogg' },
+    callback = function()
+        local filename = vim.fn.expand('%:p')
+
+        vim.fn.jobstart({ 'setsid', 'mpv', filename }, { detach = true })
+
+        vim.cmd('setlocal buftype=nofile')
+        vim.cmd('setlocal bufhidden=wipe')
+
+        vim.schedule(function()
+            vim.cmd('bd!')
+        end)
     end,
 })
